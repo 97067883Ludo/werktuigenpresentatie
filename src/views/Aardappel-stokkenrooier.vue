5f26ba9ee25077afd9e1bad21e0ef9239c1e9aed
ludotielbeke <ludotielbeke@gmail.com>
2024-03-24 12:22:12 +0100

    moved the terug button

--- src/views/Aardappel-stokkenrooier.vue
+++ src/views/Aardappel-stokkenrooier.vue
@@ -4,7 +4,7 @@
 
 <template>
   <div class="w-full h-full">
-    <router-link to="/" class="absolute ml-2 text-white bg-blue-500 p-3 text-xl">Terug</router-link>
+    <router-link to="/" class="absolute ml-2 text-white bg-blue-500 p-3 text-xl right-3 w-60 flex justify-center"><h1>Terug</h1></router-link>
     <iframe
         src="https://sway.cloud.microsoft/s/fPekjMJjEcbTH8aC/embed"
         frameborder="0"
