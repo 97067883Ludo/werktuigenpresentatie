a48fb9ea5e0ee78941cfbdbee4c7ae62bb45ab67
Ludo Tielbeke <ludotielbeke@gmail.com>
2024-03-24 11:16:12 +0100

    added icons

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,12 +3,21 @@
 
 <template>
   <div class="flex flex-wrap">
-
+    <div  class="flex flex-wrap h-full w-full">
     <div class="w-1/2 h-1/2 flex justify-center items-center">
-      <router-link class="p-4 bg-blue-500 text-white rounded-2xl" to="/melken">Melken</router-link>
+      <router-link class=" bg-blue-500 text-white rounded" to="/melken">
+        <div>
+          <img src="https://delaarman.nl/wp-content/uploads/2024/02/melken-1.jpg" alt="Afbeelding Melken" class="rounded h-30 w-30">
+          <h2 class="flex justify-center font-bold text-xl">Melken</h2>
+        </div>
+      </router-link>
     </div>
     <div class="w-1/2 h-1/2 flex justify-center items-center">
-      <router-link class="p-4 bg-blue-500 text-white rounded-2xl" to="/Aardappel-stokkenrooier">Aardappel-stokkenrooier</router-link>
+      <router-link class="bg-blue-500 text-white rounded flex" to="/Aardappel-stokkenrooier"><div>
+          <img src="https://delaarman.nl/wp-content/uploads/2024/02/Aardappel-stokkenrooier-300x272.jpg" alt="Afbeelding Aardappel-stokkenrooier" class="rounded h-30 w-30">
+          <h2 class="flex justify-center font-bold text-xl"> Aardappel-stokkenrooier</h2>
+        </div>
+      </router-link>
     </div>
     <div class="w-1/2 h-1/2 flex justify-center items-center">
       <router-link class="p-4 bg-blue-500 text-white rounded-2xl" to="/about">Melken</router-link>
@@ -16,5 +25,6 @@
     <div class="w-1/2 h-1/2 flex justify-center items-center">
       <router-link class="p-4 bg-blue-500 text-white rounded-2xl" to="/about">Melken</router-link>
     </div>
+  </div>
   </div>
 </template>
